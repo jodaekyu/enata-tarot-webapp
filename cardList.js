c83715490224d00fd7d5fecc59478eb1f864b0f2
jodaekyu <jodacu001@gmail.com>
2025-05-29 01:39:31 +0900

    Add files via upload

---
+++ cardList.js
@@ -0,0 +1,51 @@
+const cardList = [
+  {
+    name: "The Fool",
+    korean_name: "바보",
+    position: "정방향",
+    summary: "연애에 미숙하고 책임감 부족. 깊은 관계로 발전하기 어려우며, 재회 가능성도 낮음."
+  },
+  {
+    name: "The Magician",
+    korean_name: "마법사",
+    position: "정방향",
+    summary: "자신감 있고 능력 있는 사람. 끌림은 강하지만 관계는 불안정할 수 있음."
+  },
+  {
+    name: "The High Priestess",
+    korean_name: "여교황",
+    position: "정방향",
+    summary: "감정을 드러내지 않으며 신중한 태도. 관계 진전엔 시간이 걸림."
+  },
+  {
+    name: "The Empress",
+    korean_name: "여제",
+    position: "정방향",
+    summary: "풍요롭고 다정한 사람. 연애운이 좋고, 결혼 운도 강함."
+  },
+  {
+    name: "The Emperor",
+    korean_name: "황제",
+    position: "정방향",
+    summary: "의지가 강하고 주도적인 사람. 믿음직하지만 다소 권위적일 수 있음."
+  },
+  {
+    name: "The Hierophant",
+    korean_name: "교황",
+    position: "정방향",
+    summary: "소개로 시작되는 인연. 안정적인 연애로 발전 가능."
+  },
+  {
+    name: "The Lovers",
+    korean_name: "연인",
+    position: "정방향",
+    summary: "감정 교류가 풍부하고 로맨틱한 관계. 진지한 만남으로 발전할 수 있음."
+  },
+  {
+    name: "The Chariot",
+    korean_name: "전차",
+    position: "정방향",
+    summary: "연애에 대한 열정이 강함. 빠르게 진전될 수 있으나 갈등도 동반될 수 있음."
+  }
+  // … 이후 카드도 동일 형식으로 추가됨
+];
